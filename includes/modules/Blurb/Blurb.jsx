807495b5314c473a5475927df32af7ef71fb1112
nishanmazumder <arosh019@gmail.com>
2022-12-03 23:36:19 +0600

    advance config added

--- includes/modules/Blurb/Blurb.jsx
+++ includes/modules/Blurb/Blurb.jsx
@@ -7,39 +7,45 @@ import "./style.css";
 class NM_BLURB extends Component {
   static slug = "nm_divi_blurb";
 
+  constructor(props){
+    super(props)
+
+    console.log(props)
+  }
+
   render() {
     const Title = this.props.nm_title;
-    const Content = this.props.content();
+    // const Content = this.props.content();
+    const Content = this.props.nm_content;
+    const Image = this.props.nm_img;
+    const Button_Txt = this.props.nm_btn;
+    const Button_Url = this.props.nm_btn_url;
+
+
+
 
     return (
       <>
-        <div className="nm_text_bloc">
-          <h1>{Title}</h1>
-          <p>{Content}</p>
-        </div>
-
-        <div class="featured-box featured-box-default">
-        <div class="featured-box-image">
-          <div class="image-mask">
-            <img src="#" alt="Tree Plantation System" />
+        <div className="featured-box featured-box-default">
+          <div className="featured-box-image">
+            <div className="image-mask">
+              <img src={Image} alt="Tree Plantation System" />
+            </div>
+          </div>
+          <div className="featured-box-content">
+            <h3 className="featured-box-title">
+              <span>{Title}</span>
+            </h3>
+            <div className="featured-box-text">
+            {Content}
+            </div>
+            <div className="featured-box-button">
+              <a className="featured-box-readmore display-inline-block" href={Button_Url}>
+              {Button_Txt}
+              </a>
+            </div>
           </div>
         </div>
-        <div class="featured-box-content">
-          <h3 class="featured-box-title">
-            <span>Tree Plantation System </span>
-          </h3>
-          <div class="featured-box-text">
-            Click edit button to change this text. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper
-            mattis, pulvinar dapibus leo.
-          </div>
-          <div class="featured-box-button">
-            <a class="featured-box-readmore display-inline-block" href="#">
-              Read More
-            </a>
-          </div>
-        </div>
-      </div>
       </>
     );
   }
--- includes/modules/Blurb/Blurb.php
+++ includes/modules/Blurb/Blurb.php
@@ -23,12 +23,72 @@ class NM_BLURB extends ET_Builder_Module
 		$this->settings_modal_toggles = [
 			'general' => [
 				'toggles' => [
-					'content' => esc_html('Content', 'nmdivi-divi-custom-mod'),
-					'image' => esc_html('Image', 'nmdivi-divi-custom-mod'),
-					'button' => esc_html('Button', 'nmdivi-divi-custom-mod')
+					'title' => esc_html__('Title', 'nmdivi-divi-custom-mod'),
+					'content' => esc_html__('Content', 'nmdivi-divi-custom-mod'),
+					'image' => esc_html__('Image', 'nmdivi-divi-custom-mod'),
+					'button' => esc_html__('Button', 'nmdivi-divi-custom-mod')
+				]
+			],
+
+			'advanced' => [
+				'toggles' => [
+					'title' => esc_html__('Title', 'nmdivi-divi-custom-mod'),
+					'content' => esc_html__('Content', 'nmdivi-divi-custom-mod'),
+					'image' => esc_html__('Image', 'nmdivi-divi-custom-mod'),
+					'button' => esc_html__('Button', 'nmdivi-divi-custom-mod')
 				]
 			]
 		];
+	}
+
+	public function get_advanced_fields_config()
+	{
+		$advanced_fields = array();
+
+		$advanced_fields['fonts'] = array(
+			'title'   => array(
+				'label'         => esc_html__('Title', 'nmdivi-divi-custom-mod'),
+				'toggle_slug'   => 'title',
+				'tab_slug'        => 'advanced',
+				'line_height' => array(
+					'default' => '1em',
+				),
+				'font_size' => array(
+					'default' => '24px',
+				),
+				'font-weight' => array(
+					'default' => 'bold'
+				),
+				'css'      => array(
+					'main' => " %%order_class%% .featured-box-title",
+					//'hover' => "%%order_class%% .featured-box-title:hover .featured-box-title",
+					'hover' => "%%order_class%% .featured-box-title:hover",
+					'important' => 'all',
+				),
+				'header_level' => array(
+					'default' => 'h3',
+				),
+			),
+
+			'content'   => array(
+                'label'         => esc_html__('Content', 'divi_flash'),
+                'toggle_slug'   => 'content',
+                'sub_toggle'  => 'body',
+                'line_height' => array(
+                    'default' => '1em',
+                ),
+                'font_size' => array(
+                    'default' => '14px',
+                ),
+                'css'      => array(
+                    'main' => "%%order_class%% .featured-box-text",
+                    'hover' => "%%order_class%% .featured-box-text",
+                    'important' => 'all',
+                ),
+            ),
+		);
+
+		return $advanced_fields;
 	}
 
 
@@ -53,9 +113,9 @@ class NM_BLURB extends ET_Builder_Module
 			'nm_img' => array(
 				'label'           => esc_html__('Image', 'nmdivi-divi-custom-mod'),
 				'type'            => 'upload',
-				'upload_button_text' => esc_attr__( 'Upload an image', 'et_builder' ),
-				'choose_text'        => esc_attr__( 'Choose an Image', 'et_builder' ),
-				'update_text'        => esc_attr__( 'Set As Image', 'et_builder' ),
+				'upload_button_text' => esc_attr__('Upload an image', 'et_builder'),
+				'choose_text'        => esc_attr__('Choose an Image', 'et_builder'),
+				'update_text'        => esc_attr__('Set As Image', 'et_builder'),
 				'option_category' => 'basic_option',
 				'description'     => esc_html__('', 'nmdivi-divi-custom-mod'),
 				'toggle_slug'     => 'image',
@@ -74,15 +134,69 @@ class NM_BLURB extends ET_Builder_Module
 				'description'     => esc_html__('', 'nmdivi-divi-custom-mod'),
 				'toggle_slug'     => 'button',
 			),
+			'button_url_new_window' => array(
+				'default'         => 'off',
+				'default_on_front' => true,
+				'label'           => esc_html__('Url Mode', 'dicm-divi-custom-modules'),
+				'type'            => 'select',
+				'option_category' => 'configuration',
+				'options'         => array(
+					'off' => esc_html__('In The Same Window', 'dicm-divi-custom-modules'),
+					'on'  => esc_html__('In The New Tab', 'dicm-divi-custom-modules'),
+				),
+				'toggle_slug'     => 'button',
+				'description'     => esc_html__('', 'dicm-divi-custom-modules'),
+			),
 		);
 	}
 
 	public function render($attrs, $content = null, $render_slug)
 	{
-		$title = sprintf('<h1>%1$s</h1>', $this->props['nm_title']);
-		$content = sprintf('<p>%1$s</p>', $this->props['content']);
 
-		return $title . $content;
+
+		// Content
+		$title = $this->props['nm_title'];
+		$content = $this->props['content'];
+
+		//Image
+		$img = $this->props['nm_img'];
+
+		//Button
+		$btn_txt = $this->props['nm_btn'];
+		$btn_url = $this->props['nm_btn_url'];
+
+		// $button = $this->render_button([
+		// 	'button_text'      => $btn_txt,
+		// 	'button_url'       => $btn_url,
+		// ]);
+
+		$output = sprintf(
+			'        <div class="test featured-box featured-box-default">
+			<div class="featured-box-image">
+			  <div class="image-mask">
+				<img src="%3$s" alt="Tree Plantation System" />
+			  </div>
+			</div>
+			<div class="featured-box-content">
+			  <h3 class="featured-box-title">
+				<span>%1$s</span>
+			  </h3>
+			  <div class="featured-box-text">%2$s</div>
+			  <div class="featured-box-button">
+				<a class="featured-box-readmore display-inline-block" href="%4$s">
+				%5$s
+				</a>
+			  </div>
+			</div>
+		  </div>',
+			esc_html__($title),
+			esc_html__($content),
+			$img,
+			esc_url($btn_url),
+			esc_html__($btn_txt)
+		);
+
+		return $output;
 	}
 }
 
